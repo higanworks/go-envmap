7e90759a924399ccc3a2b271834adc31a911f417
sawanoboly <sawanoboriyu@higanworks.com>
2014-12-26 02:05:49 +0900

    add testify to test.

---
+++ envmap_test.go
@@ -0,0 +1,19 @@
+package envmap
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestAll(t *testing.T) {
+	oldOsEnv := OsEnv
+	OsEnv = func() []string {
+		var hoge []string
+		hoge = append(hoge, "HOGE=hogehoge")
+		return hoge
+	}
+
+	// assert equality
+	assert.Equal(t, All(), map[string]string{"HOGE": "hogehoge"}, "they should be equal")
+	OsEnv = oldOsEnv
+}
